48faa1660a9c231f22472897952f4e3e3ee3919a
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-01-28 18:22:53 +0500

    Create connector.js

---
+++ conect_vue/connector.js
@@ -0,0 +1,10 @@
+var mathjs = new Vue({
+  el: "conect_app",
+  data: function() {
+    return: {
+      conect = "Math.js succses conect",
+      port = "mathjs",
+      console = ">_"
+    }
+}
+});
