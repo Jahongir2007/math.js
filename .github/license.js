c856cf9fa2e2ac49a4087facb2b4b4b9fddfc31c
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-01-28 19:03:25 +0500

    Create license.js

---
+++ .github/license.js
@@ -0,0 +1,10 @@
+var license = new Vue({
+  el: "#license_mathjs",
+  data: function(){
+    return: {
+      for_pay: "free",
+      license: "MIT",
+      author: "Jahongir Sobirov"
+    },
+  }
+});
